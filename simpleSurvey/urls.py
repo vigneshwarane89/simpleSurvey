503dc91b218385c9277924ccbeac2269d6b863ea
vigneshwarane89 <vigneshwarane89@gmail.com>
2020-07-31 12:29:01 +0530

    Models are created.

--- simpleSurvey/urls.py
+++ simpleSurvey/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from survey import urls as appurls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(appurls),
 ]
--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Survey(models.Model):
+    title = models.CharField(max_length = 300)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 900)
+    survey = models.ForiegnKey(Survey)
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length = 900)
+    question = models.ForiegnKey(Question)
+
+class SurveyAnswer(models.Model):
+    orig_survey = models.ForiegnKey(Survey)
+
+class QuestionAnswer(models.Model):
+    answer = models.ForiegnKey(Choice)
+    survey_answer = models.ForiegnKey(Survey)
