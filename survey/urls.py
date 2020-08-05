6ffc3a1f613f827cb1550676188b96191bfd8a9f
vigneshwarane89 <vigneshwarane89@gmail.com>
2020-08-05 11:35:08 +0530

    Further views and templates were created.

--- survey/urls.py
+++ survey/urls.py
@@ -4,6 +4,8 @@ import views
 
 urlpatterns = [
     path(" ",views.index,name= "root"),
+    path("survey_load/", views.load_survey, name = "load-survey"),
+    path(r"^survey/(?P<survey_id>\d+)/$", views.survey_view, name = "survey-detail"),
 
 
 
--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
-
+from models import Survey,SurveyAnswer
+from django.contrib.auth import authenticate,login
 # Create your views here.
+def index(request):
+    ctx={}
+
+    return render(request,"main.html",ctx)
+
+def survey_view(request,survey_id= None):
+    try:
+        survey = Survey.objects.get(id=survey_id)
+        questions = survey.question_set.all()
+        ctx={'survey':survey, 'questions': questions}
+    except:
+        return render(request, 'surveynotfound-error.html',{'sv_id': survey_id})
+    return render(request, 'suvey-take.html',ctx)
+
+def load_survey(request):
+    sv_to_load = request.POST['survey_view']
+    return redirect('survey-detail',survey_id = sv_to_load)
+
+def survey_fill(request):
+    answer= SurveyAnswer()
+    orig_survey = Survey.objects.get(id=request.POST['survey_id'])
+    answer.orig_survey = orig_survey
+    answer.save()
+    questions = orig_survey.question_set.all()
+
+    for question in questions:
+        question_post = request.POST['question' + str(question.id)]
+        QA = QuestionAnswer()
+        QA.answer.answer = Choice.objects.get(id = int(question_post))
+        QA.survey_answer = answer
+        QA.save()
+    answer.save()
+    return render(request,'survey-complete.html',{})
+
+def admin_login(request):
+    admin_username = request.POST['username']
+    admin_password = request.POST['password']
+    user = authenticate(username = admin_password,password = admin_password)
+    if user is not None:
+        login(request,user)
+        redirect('admin-panel')
+    return render(request,'main.html',{'login':False})
+
+def admin-panel(request):
+    surveys = Survey.objects.all()
+    ctx = {'surveys': surveys}
+    return render(request,'admin-panel.html',ctx)
